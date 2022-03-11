bc2985ace57c17928321a7a02161c3b5ba42f636
Gianluca De Rossi <gianluca.derossi@microgate.it>
2022-03-11 15:41:31 +0100

    Basic project

---
+++ SouthAfricanNumbers.DomainModel/Data/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SouthAfricanNumbers.DomainModel
+{
+    public class Number
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
